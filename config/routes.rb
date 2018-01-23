e86c64d091f9090478085c0e32fc5e16c7e96457
xiaowei <shenzhoudance@gmail.com>
2018-01-23 19:52:59 +0800

    add 20180123

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root "products#new"
+  root "welcome#index"
   resources :products
-
   resources :product_lists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
