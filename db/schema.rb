424acc7ad656da8f5987dc2947b9d34551fcdab3
Zhang-jia-chen <zjc834723074@gmail.com>
2018-01-30 16:06:09 +0800

    add something

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180129061345) do
 
   create_table "product_list_contents", force: :cascade do |t|
